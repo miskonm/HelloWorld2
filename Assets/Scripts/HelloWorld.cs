f760f8fe00c7f3b919fce876a0baf3394821ebb9
Nikita Misko <miskonm@gmail.com>
2021-03-25 20:52:53 +0300

    Init project

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    public int min = 5;
+    public int max = 750;
+    
+    private int guess;
+    
+    private void Start()
+    {
+        print("Загадай число от " + min + " до " + max);
+        UpdateGuess();
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = guess;
+            UpdateGuess();
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            max = guess;
+            UpdateGuess();
+        }
+        if (Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return))
+        {
+            print("Конец игры.");
+        }
+    }
+
+    private void UpdateGuess()
+    {
+        guess = (min + max) / 2;
+        print("Ты загадал " + guess + "?");
+    }
+}
